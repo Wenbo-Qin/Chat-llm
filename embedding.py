ad8c6955a953b4d5e4c6899de3020a4533f11673
Wenbo Qin <iallthewaynorth@gmail.com>
2025-10-28 23:29:13 +0800

    add embedding function

--- embedding.py
+++ embedding.py
@@ -2,14 +2,16 @@ import dashscope
 from http import HTTPStatus
 from env import *
 
-input_text = "衣服的质量杠杠的"
-resp = dashscope.TextEmbedding.call(
-    model="text-embedding-v4",
-    input=input_text,
-    api_key=qwen_api_key
-)
 
-if resp.status_code == HTTPStatus.OK:
-    # print(type(resp))
-    print(resp.output)
-    print(resp.output['embeddings'][0]['embedding'])
+def embedding(input_text: str = "衣服的质量杠杠的"):
+    resp = dashscope.TextEmbedding.call(
+        model="text-embedding-v4",
+        input=input_text,
+        api_key=qwen_api_key
+    )
+
+    if resp.status_code == HTTPStatus.OK:
+        # print(type(resp))
+        # print(resp.output)
+        # print(resp.output['embeddings'][0]['embedding'])
+        return resp.output['embeddings'][0]['embedding']
