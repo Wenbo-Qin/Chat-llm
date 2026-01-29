35a4c856e7f6077fc9bb8a1127bcd286b9afc225
Wenbo Qin <iallthewaynorth@gmail.com>
2026-01-29 10:04:56 +0800

    refact code

--- embedding_service/embedding_processor.py
+++ embedding_service/embedding_processor.py
@@ -14,8 +14,11 @@ def embedding(input_text: str = "衣服的质量杠杠的"):
     )
 
     if resp.status_code == HTTPStatus.OK:
-        print("Embedding successful\n")
+        # print("Embedding successful\n")
         return resp.output['embeddings'][0]['embedding']
+    else:
+        print(f"Embedding failed: {resp.status_code} - {resp.message}")
+        return None
 
 if __name__ == "__main__":
     print(embedding())
